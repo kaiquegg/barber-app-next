f37c5214fa94b124e2cdc72c100090baa00465e2
Kaíque Egg <kaiquegg2@hotmail.com>
2025-09-16 17:44:04 -0300

    chore: add page components

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./components/header";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
 import Image from "next/image";
+import { Card, CardContent } from "./components/ui/card";
+import { Badge } from "./components/ui/badge";
+import { Avatar, AvatarImage } from "./components/ui/avatar";
 
 const Home = () => {
   return (
@@ -11,13 +14,15 @@ const Home = () => {
       <div className="p-5">
         <h2 className="text-xl font-bold">Olá, Usuário(a)!</h2>
         <p>Quarta-feira, 27 de agosto.</p>
-
+        {/* INPUT DE BUSCA */}
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Faça sua busca..." />
           <Button variant="outline">
             <SearchIcon />
           </Button>
         </div>
+
+        {/* Imagem */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende nos melhores"
@@ -26,6 +31,31 @@ const Home = () => {
             className="rounded-xl object-cover"
           />
         </div>
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* left */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src=" https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia Belo Horizonte</p>
+              </div>
+            </div>
+
+            {/* right */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">30</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
+        {/* FIM DIV P-5 */}
       </div>
     </div>
   );
