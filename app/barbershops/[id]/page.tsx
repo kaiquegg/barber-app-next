9fee563bc29a22e2bfaaf59bbd6363ddb52358a2
Kaíque Egg <kaiquegg2@hotmail.com>
2025-10-21 10:57:55 -0300

    feat: add initial barbershop page

---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,75 @@
+import { Button } from "@/app/components/ui/button";
+import { db } from "@/app/lib/prisma";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+interface BarbershopPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const BarbershopPage = async ({ params }: BarbershopPageProps) => {
+  // call db
+  const barbershop = await db.barberShop.findUnique({
+    where: {
+      id: params.id,
+    },
+  });
+  if (!barbershop) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      {/* IMAGEM */}
+      <div className="relative h-[250px] w-full">
+        <Image
+          alt={barbershop?.name}
+          src={barbershop?.imageUrl}
+          fill
+          className="object-cover"
+        />
+        <Button
+          size={"icon"}
+          variant="secondary"
+          className="absolute top-4 left-4"
+          asChild
+        >
+          <Link href="/">
+            <ChevronLeftIcon />
+          </Link>
+        </Button>
+        <Button
+          size={"icon"}
+          variant="secondary"
+          className="absolute top-4 right-4"
+          asChild
+        >
+          <MenuIcon />
+        </Button>
+      </div>
+
+      <div className="border-b border-solid p-5">
+        <h1 className="mb-3 text-xl font-bold">{barbershop?.name}</h1>
+        <div className="mb-2 flex items-center gap-2">
+          <MapPinIcon className="text-primary" size={18} />
+          <p className="text-sm">{barbershop?.address}</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <StarIcon className="text-primary fill-primary" size={18} />
+          <p className="text-sm">5,0 (499 avaliações)</p>
+        </div>
+      </div>
+      {/* Description  */}
+      <div className="space-y-2 border-b border-solid p-5">
+        <h2 className="text-xs font-bold text-gray-400 uppercase">Sobre nós</h2>
+        <p className="text-justify text-sm">{barbershop?.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default BarbershopPage;
--- app/components/barbershop-item.tsx
+++ app/components/barbershop-item.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { Badge } from "./ui/badge";
 import { StarIcon } from "lucide-react";
+import Link from "next/link";
 
 interface BarberShopItemProps {
   barbershop: BarberShop;
@@ -35,8 +36,8 @@ const BarbershopItem = ({ barbershop }: BarberShopItemProps) => {
             {barbershop.name}
           </h3>
           <p className="truncate text-sm text-gray-400">{barbershop.address}</p>
-          <Button variant="secondary" className="mt-3 w-full">
-            Reservar
+          <Button variant="secondary" className="mt-3 w-full" asChild>
+            <Link href={`/barbershops/${barbershop.id}`}>Reservar</Link>
           </Button>
         </div>
       </CardContent>
